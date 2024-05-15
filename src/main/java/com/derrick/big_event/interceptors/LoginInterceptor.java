840d6b249cd3116582122469f883bf5f2b2f2c5c
Derrick Chen <derrick.chen@coyote-x.com>
2024-05-14 23:35:16 -0400

    Create login interceptor class

---
+++ src/main/java/com/derrick/big_event/interceptors/LoginInterceptor.java
@@ -0,0 +1,26 @@
+package com.derrick.big_event.interceptors;
+
+import com.derrick.big_event.pojo.Result;
+import com.derrick.big_event.utils.JwtUtil;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.util.Map;
+
+@Component
+public class LoginInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String token = request.getHeader("Authorization");
+        try {
+            Map<String, Object> claims = JwtUtil.parseToken(token);
+            return true;
+        } catch (Exception e) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return false;
+        }
+    }
+}
