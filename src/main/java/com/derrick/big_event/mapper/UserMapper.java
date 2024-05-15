9481092e5794077583629973d4c97a01e985da38
Derrick Chen <derrick.chen@coyote-x.com>
2024-05-15 10:56:04 -0400

    Create update user avatar and password functions to UserMapper class

--- src/main/java/com/derrick/big_event/mapper/UserMapper.java
+++ src/main/java/com/derrick/big_event/mapper/UserMapper.java
@@ -4,6 +4,7 @@ import com.derrick.big_event.pojo.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 @Mapper
 public interface UserMapper {
@@ -26,4 +27,28 @@ public interface UserMapper {
     @Insert("INSERT INTO user(username, password, create_time, update_time)" +
             " VALUES (#{username}, #{encryptedPwd}, now(), now())")
     void saveNewUser(String username, String encryptedPwd);
+
+    /**
+     * Update user information
+     *
+     * @param user
+     */
+    @Update("UPDATE user SET nickname=#{nickname}, email=#{email}, update_time=#{updateTime} WHERE id = #{id}")
+    void updateUserInfo(User user);
+
+    /**
+     * Update user avatar
+     *
+     * @param avatarUrl
+     */
+    @Update("UPDATE user SET user_pic=#{avatarUrl}, update_time = now() WHERE id = #{userId}")
+    void updateAvatar(String avatarUrl, Integer userId);
+
+    /**
+     * Update user password
+     *
+     * @param pwd
+     */
+    @Update("UPDATE user SET password=#{pwd}, update_time = now() WHERE id = #{userId}")
+    void updatePwd(String pwd, Integer userId);
 }
