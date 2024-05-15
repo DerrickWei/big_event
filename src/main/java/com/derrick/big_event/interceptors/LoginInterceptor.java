037dfc4c79c5fd81bd20318dd583167d7a2654e9
Derrick Chen <derrick.chen@coyote-x.com>
2024-05-15 10:06:27 -0400

    Use ThreadLocal to get user info from parsed JWT

--- src/main/java/com/derrick/big_event/interceptors/LoginInterceptor.java
+++ src/main/java/com/derrick/big_event/interceptors/LoginInterceptor.java
@@ -2,6 +2,7 @@ package com.derrick.big_event.interceptors;
 
 import com.derrick.big_event.pojo.Result;
 import com.derrick.big_event.utils.JwtUtil;
+import com.derrick.big_event.utils.ThreadLocalUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
@@ -16,11 +17,22 @@ public class LoginInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String token = request.getHeader("Authorization");
         try {
+            // Get user info from JWT
             Map<String, Object> claims = JwtUtil.parseToken(token);
+
+            // Save claims to ThreadLocal
+            ThreadLocalUtil.set(claims);
+
             return true;
         } catch (Exception e) {
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
             return false;
         }
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        // Clear ThreadLocal data
+        ThreadLocalUtil.remove();
     }
 }
