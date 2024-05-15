c7217df69744934244ddd54bcc8abe865ce7c011
Derrick Chen <derrick.chen@coyote-x.com>
2024-05-14 23:49:00 -0400

    Json ignore user password

--- src/main/java/com/derrick/big_event/pojo/User.java
+++ src/main/java/com/derrick/big_event/pojo/User.java
@@ -1,18 +1,21 @@
 package com.derrick.big_event.pojo;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
 public class User {
-    private Integer id;//主键ID
-    private String username;//用户名
-    private String password;//密码
-    private String nickname;//昵称
-    private String email;//邮箱
-    private String userPic;//用户头像地址
-    private LocalDateTime createTime;//创建时间
-    private LocalDateTime updateTime;//更新时间
+    private Integer id;      // 主键ID
+    private String username; // 用户名
+
+    @JsonIgnore
+    private String password; // 密码
+    private String nickname; // 昵称
+    private String email;    // 邮箱
+    private String userPic;  // 用户头像地址
+    private LocalDateTime createTime;   // 创建时间
+    private LocalDateTime updateTime;   // 更新时间
 }
