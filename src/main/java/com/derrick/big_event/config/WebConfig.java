e69c7d57f627f3d6eb97c2d428f5651921cf4576
Derrick Chen <derrick.chen@coyote-x.com>
2024-05-14 23:35:42 -0400

    Create Web config for registering login interceptor

---
+++ src/main/java/com/derrick/big_event/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.derrick.big_event.config;
+
+import com.derrick.big_event.interceptors.LoginInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private LoginInterceptor loginInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor)
+                .excludePathPatterns("/api/v1/user/login", "/api/v1/user/register");
+    }
+}
